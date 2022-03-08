716715451f03a320548a1ef5732c9432f7304139
ArtemSap <artik21rus21@gmail.com>
2022-03-08 16:15:03 +0300

    Challenge 2 is done

---
+++ Challange 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float TimeBetweenSpawning = 1;
+
+    private float timer = 0.0f;
+    private bool pressed = false;
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !pressed)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            pressed = true;
+        }
+
+        if (pressed)
+            timer += Time.deltaTime;
+
+        if (timer > TimeBetweenSpawning)
+        {
+            timer = 0;
+            pressed = false;
+        }
+    }
+}
